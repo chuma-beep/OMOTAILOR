7242319d7f63b03d585a027a8471cb4751265ae7
chuma-beep <wisboynelson123@gmail.com>
2023-05-06 13:02:35 +0000

    updates made
    updates

--- login-signup.js
+++ login-signup.js
@@ -1,8 +1,10 @@
+// varaibles const= immutable irrefractorable. var let
 let signup = document.querySelector(".signup");
 let login = document.querySelector(".login");
 let slider = document.querySelector(".slider");
 let formSection = document.querySelector(".form-section");
 
+// slider function for login.
 signup.addEventListener("click", () => {
 	slider.classList.add("moveslider");
 	formSection.classList.add("form-section-move");
@@ -12,3 +14,4 @@ login.addEventListener("click", () => {
 	slider.classList.remove("moveslider");
 	formSection.classList.remove("form-section-move");
 });
+
